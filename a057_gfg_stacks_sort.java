465d1e23dfaacb23cf89cbd2a5ae125386008e96
anshuman patel <bladorunnero@gmail.com>
2018-02-08 20:24:42 +0000

    sort a strack

---
+++ a057_gfg_stacks_sort.java
@@ -0,0 +1,45 @@
+import java.util.Random;
+import java.util.Stack;
+
+public class a057_gfg_stacks_sort {
+
+	static Stack<Integer> s = new Stack<>(); 
+	
+	public static void main(String args[]) {
+		Random r = new Random();
+		
+		for(int i=0; i<15; i++) {
+			s.push(r.nextInt(100));
+		}
+		
+		System.out.println(s);
+		
+		sort(s);
+		
+		System.out.println(s);
+		
+	}
+
+	private static void sort(Stack<Integer> s) {
+		// TODO Auto-generated method stub
+		if(!s.isEmpty()) {
+			int x = s.pop();
+			sort(s);
+			sorted_insert(s,x);
+		}
+	}
+
+	private static void sorted_insert(Stack<Integer> ss, int x) {
+		// TODO Auto-generated method stub
+		if (s.isEmpty() || x>s.peek()) {
+			s.push(x);
+			return;
+		}else {
+			int t = s.pop();
+			sorted_insert(s,x);
+			s.push(t);
+		}
+			
+	}
+	
+}
