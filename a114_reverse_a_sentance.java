065137659a8787e4b75eefcaf0c8a52f9c96514f
anshuman patel <bladorunnero@gmail.com>
2018-03-21 22:31:35 +0530

    reverse a sentance

---
+++ a114_reverse_a_sentance.java
@@ -0,0 +1,14 @@
+
+public class a114_reverse_a_sentance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String sent = "I am practicing this small coding problems now.",senttt = "";
+		String[] sentt = sent.split(" ");
+		for(int i = sentt.length-1; i>=0; i--) {
+			senttt += sentt[i]+" ";
+		}
+		System.out.println(senttt);
+	}
+
+}
