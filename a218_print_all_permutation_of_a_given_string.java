de6f46805d9ae3fb8af1f43c3e558dd6fda528dc
anshuman patel <bladorunnero@gmail.com>
2018-06-01 08:45:20 +0530

    print all permutation of a given string

---
+++ a218_print_all_permutation_of_a_given_string.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package new_package;
+
+/**
+ *
+ * @author bladeRUNNER
+ */
+public class a218_print_all_permutation_of_a_given_string {
+     public static void main(String[] args)
+    {
+        String str = "ABCDEF";
+        int n = str.length();
+        permute(str, 0, n-1);
+    }
+ 
+    
+    static void permute(String str, int l, int r)
+    {
+        if (l == r)
+            System.out.println(str);
+        else
+        {
+            for (int i = l; i <= r; i++)
+            {
+                str = swap(str,l,i);
+                permute(str, l+1, r);
+                str = swap(str,l,i);
+            }
+        }
+    }
+ 
+    static String swap(String a, int i, int j)
+    {
+        char temp;
+        char[] charArray = a.toCharArray();
+        temp = charArray[i] ;
+        charArray[i] = charArray[j];
+        charArray[j] = temp;
+        return String.valueOf(charArray);
+    }
+}
