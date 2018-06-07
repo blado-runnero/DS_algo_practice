4241dcc0ce52ad2bdc1cac673cedee3b26fcaeca
anshuman patel <bladorunnero@gmail.com>
2018-06-07 06:40:25 +0530

    find all the groups of words which are anagrams

---
+++ a221_anagrams.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package new_package;
+
+import java.util.*;
+
+/**
+ *
+ * @author bladeRUNNER
+ */
+public class a221_anagrams {
+      public static ArrayList<ArrayList<Integer>> anagrams(final List<String> a) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        HashMap<String, ArrayList<Integer>> map = new HashMap<String, ArrayList<Integer>>();
+        
+        for(int i = 0; i < a.size(); i++){
+            char[] c = a.get(i).toCharArray();
+            Arrays.sort(c);
+            String t = String.valueOf(c);
+            if(map.get(t) == null){
+                ArrayList<Integer> l = new ArrayList<Integer>();
+                l.add(i+1);
+                map.put(t, l);
+            }
+            else
+                map.get(t).add(i + 1);
+        }
+        for(ArrayList<Integer> l : map.values()){
+                result.add(l);
+        }
+        
+        return result;
+ }
+    public static void main(String[] args){
+        List<String> a = new ArrayList<String>();
+        a.add("cat");
+        a.add("dog");
+        a.add("god");
+        a.add("atc");
+        ArrayList<ArrayList<Integer>> result = anagrams(a);
+        for(int i = 0; i < result.size(); i++){
+            System.out.print("[");
+            for(int j : result.get(i))
+                System.out.print((j) + " ");
+            System.out.println("]");
+        }
+    }
+}
